37c64fbaaccc4248bff16fbd6ce6c592d0a275d5
Süleyman Özgür Özarpacı <oozarpac@gmail.com>
2023-03-11 22:54:24 +0300

    updated invoice table row

--- resources/js/components/InvoiceTableRow.vue
+++ resources/js/components/InvoiceTableRow.vue
@@ -1,28 +1,30 @@
 <template>
     <div
         @click="goToInvoice(invoice.id)"
-        class="flex flex-row justify-center items-center bg-white shadow px-7 py-5 rounded-lg mb-4 cursor-pointer h-full text-[15px]"
+        class="flex rounded-lg mb-4 cursor-pointer bg-white shadow  items-center p-6"
     >
-        <div class="w-1/6 h-full">
-            <span class="font-bold text-lg">
-                <span class="text-gray-400">#</span>
-                {{ invoice.id.toUpperCase() }}
-            </span>
-        </div>
-        <div class="w-1/6 text-custom-seven">
-            Due {{ dayjs(invoice.payment_due).format('DD MMM YYYY') }}
-        </div>
-        <div class="w-1/6 h-full text-custom-seven">
-            {{ invoice.client_name }}
-        </div>
-        <div class="w-1/6 h-full font-bold text-center text-lg">
-            {{ MoneyFormatter(invoice.total, '£') }}
-        </div>
-        <div class="w-1/6 h-full text-right flex justify-between justify-center items-center ">
-            <InvoiceStatusBadge :status="invoice.status" />
+        <div class="grow grid grid-cols-6 gap-4 text-sm items-center">
+            <div>
+                <span class="font-bold text-lg text-base">
+                    <span class="text-gray-400">#</span>
+                    {{ invoice.id.toUpperCase() }}
+                </span>
+            </div>
+            <div class="text-custom-seven">
+                Due {{ dayjs(invoice.payment_due).format('DD MMM YYYY') }}
+            </div>
+            <div class="col-span-2 text-custom-seven">
+                {{ invoice.client_name }}
+            </div>
+            <div class="font-bold text-right text-lg text-base">
+                {{ MoneyFormatter(invoice.total, '£') }}
+            </div>
+            <div class="text-right flex justify-between justify-center items-center ">
+                <InvoiceStatusBadge :status="invoice.status" />
+            </div>
         </div>
 
-        <div class="w-1/6 h-full">
+        <div class="flex-none h-full pl-6">
             <svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1 1L5 5L1 9" stroke="#7C5DFA" stroke-width="2"/>
             </svg>
