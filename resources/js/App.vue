791a79df31dcd405c472fb42352218d4a2b10916
Süleyman Özgür Özarpacı <oozarpac@gmail.com>
2023-03-11 14:32:08 +0300

    updated logo and profile pic in app

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="flex bg-custom-gray-light  h-full">
-        <aside class="w-44 fixed left-0 top-0 h-screen bg-slate-700 p-10">
+        <aside class="w-[103px] fixed left-0 top-0 h-screen bg-slate-700 z-50 rounded-r-[20px]">
             <div class="h-full grid grid-cols-1 content-between ...">
-                <div>LOGO</div>
-                <div>{{ auth.message }}</div>
+                <div>
+                    <img :src="Logo" alt="Logo" class="w-[103px]">
+                </div>
+                <div class="text-center border-t-2 border-[#494E6E]">
+                    <img :src="ProfilePicture" alt="Profile Picture" class="w-[40px] inline py-7">
+                </div>
             </div>
         </aside>
-        <main class="flex-1 ml-44">
+        <main class="flex-1 ml-[103px]">
             <router-view></router-view>
         </main>
     </div>
@@ -14,6 +18,8 @@
 
 <script setup>
 import { useAuthStore } from "@/stores/auth";
+import Logo from "@/assets/logo.png";
+import ProfilePicture from "@/assets/profile-pic.png";
 
 const auth = useAuthStore();
 </script>
